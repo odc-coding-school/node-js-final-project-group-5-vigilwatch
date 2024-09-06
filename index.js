bcf6da369fd3eddef36ef2a28e362ba14857e3c4
shadtorh <torhshad09@gmail.com>
2024-09-06 23:42:19 +0000

    I created two routes and two pages contact and home

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const ejs = require("ejs");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-	res.render("home.ejs");
-});
+const homeRoutes = require("./routes/homeRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+
+app.use("/", homeRoutes);
+app.use("/contact", contactRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
