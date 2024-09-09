f8358e3c5f738908d982e8d9add067e488ca7f73
SUPERIORII <adukuly461@gmail.com>
2024-09-09 21:17:20 +0000

    database is running

--- database.js
+++ database.js
@@ -1,26 +1,25 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv').config()
+const mysql2 = require('mysql2');
 
-const db = mysql.createConnection({
+
+const db = mysql2.createConnection({
     host: "localhost",
-    user: "hubwatch",
-    password:"1234",
-    database: "database.db"
-    
-});
+    user: "root",
+    password: "1234",
+    database: "hubwatch"
+})
 
-// db.connect(function(err){
-//     if(err) return console.error(err.message);
-//     return console.log('Database is connected successfully');
-// })
 
-const query = 'CREATE TABLE IF user (id INTERGER AUTO_INCREMENT PRIMARY-KEY, fullname VARCHAR(20), email VARCHAR(20) ,password VARCHAR(20))';
 
-// db.query(query, [], (err)=>{
-//     if(err) return console.log(err.message);
-//     return console.log('user table created successful');
+db.connect(function(err){
+    if(err) return console.log(err.message);
+    db.query("CREATE DATABASE IF NOT EXISTS hubwatch", function(err) {
+        if(err) return console.log(err.message);
+        return console.log('database is created successfully');
+    })
+    return console.log('database connected');
+})
 
-// })
+
 
 
 
