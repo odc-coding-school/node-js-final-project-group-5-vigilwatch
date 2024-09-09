fedd978734d25c6c1587f8e6953e766001afcea0
SUPERIORII <adukuly461@gmail.com>
2024-09-09 14:03:42 +0000

    changes

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const path = require("path");
+const db = require('./database.js');
 const app = express();
 const PORT = 3000;
 require("dotenv").config();
+
+
 
 //middle wires
 app.use(bodyParser.urlencoded({ extended: false }));
